aee2d130a63750d951b5fd29308e5deb54ce52bd
midorimato0082 <86837324+midorimato0082@users.noreply.github.com>
2023-10-18 08:00:25 +0700

    Update new features of route: route groups

--- routes/web.php
+++ routes/web.php
@@ -29,56 +29,55 @@ Route::get('register', 'RegisterController@viewRegister')->middleware('alreadyLo
 Route::post('register', 'RegisterController@register');
 Route::get('verify/{code}', 'RegisterController@verifyAccount')->name('verify');
 
-// Dashboard
-Route::get('dashboard', 'DashboardController@viewDashboard')->middleware('isLoggedIn');
 
-// User Management
-Route::get('all-user', 'UserController@viewAll')->middleware('isLoggedIn');
-Route::get('edit-user/{id}', 'UserController@viewEdit')->middleware('isLoggedIn');
-Route::post('update-user/{id}', 'UserController@update')->middleware('isLoggedIn');
-Route::post('delete-user/{id}', 'UserController@delete')->middleware('isLoggedIn');
-Route::get('add-admin', 'UserController@viewAddAdmin')->middleware('isLoggedIn');
-Route::post('save-admin', 'UserController@saveAdmin')->middleware('isLoggedIn');
+Route::middleware('isLoggedIn')->group(function () {
+    // Dashboard
+    Route::get('dashboard', 'DashboardController@viewDashboard');
 
+    // User Management
+    Route::get('all-user', 'UserController@viewAll');
+    Route::get('edit-user/{id}', 'UserController@viewEdit');
+    Route::post('update-user/{id}', 'UserController@update');
+    Route::post('delete-user/{id}', 'UserController@delete');
+    Route::get('add-admin', 'UserController@viewAddAdmin');
+    Route::post('save-admin', 'UserController@saveAdmin');
 
-// Category Management
-Route::get('all-category', 'CategoryController@viewAll')->middleware('isLoggedIn');
-Route::get('edit-category/{id}', 'CategoryController@viewEdit')->middleware('isLoggedIn');
-Route::post('update-category/{id}', 'CategoryController@update')->middleware('isLoggedIn');
-Route::post('delete-category/{id}', 'CategoryController@delete')->middleware('isLoggedIn');
-Route::get('add-category', 'CategoryController@viewAdd')->middleware('isLoggedIn');
-Route::post('save-category', 'CategoryController@save')->middleware('isLoggedIn');
+    // Category Management
+    Route::get('all-category', 'CategoryController@viewAll');
+    Route::get('edit-category/{id}', 'CategoryController@viewEdit');
+    Route::post('update-category/{id}', 'CategoryController@update');
+    Route::post('delete-category/{id}', 'CategoryController@delete');
+    Route::get('add-category', 'CategoryController@viewAdd');
+    Route::post('save-category', 'CategoryController@save');
 
-// Location Management
-Route::get('all-location', 'LocationController@viewAll')->middleware('isLoggedIn');
-Route::get('edit-location/{id}', 'LocationController@viewEdit')->middleware('isLoggedIn');
-Route::post('update-location/{id}', 'LocationController@update')->middleware('isLoggedIn');
-Route::post('delete-location/{id}', 'LocationController@delete')->middleware('isLoggedIn');
-Route::get('add-location', 'LocationController@viewAdd')->middleware('isLoggedIn');
-Route::post('save-location', 'LocationController@save')->middleware('isLoggedIn');
+    // Location Management
+    Route::get('all-location', 'LocationController@viewAll');
+    Route::get('edit-location/{id}', 'LocationController@viewEdit');
+    Route::post('update-location/{id}', 'LocationController@update');
+    Route::post('delete-location/{id}', 'LocationController@delete');
+    Route::get('add-location', 'LocationController@viewAdd');
+    Route::post('save-location', 'LocationController@save');
 
-// Review Management
-Route::get('all-review', 'ReviewController@viewAll')->middleware('isLoggedIn');
-Route::get('edit-review/{id}', 'ReviewController@viewEdit')->middleware('isLoggedIn');
-Route::post('update-review/{id}', 'ReviewController@update')->middleware('isLoggedIn');
-Route::post('delete-review/{id}', 'ReviewController@delete')->middleware('isLoggedIn');
-Route::get('add-review', 'ReviewController@viewAdd')->middleware('isLoggedIn');
-Route::post('save-review', 'ReviewController@save')->middleware('isLoggedIn');
+    // Review Management
+    Route::get('all-review', 'ReviewController@viewAll');
+    Route::get('edit-review/{id}', 'ReviewController@viewEdit');
+    Route::post('update-review/{id}', 'ReviewController@update');
+    Route::post('delete-review/{id}', 'ReviewController@delete');
+    Route::get('add-review', 'ReviewController@viewAdd');
+    Route::post('save-review', 'ReviewController@save');
 
-// News Management
-Route::get('all-news', 'NewsController@viewAll')->middleware('isLoggedIn');
-Route::get('edit-news/{id}', 'NewsController@viewEdit')->middleware('isLoggedIn');
-Route::post('update-news/{id}', 'NewsController@update')->middleware('isLoggedIn');
-Route::post('delete-news/{id}', 'NewsController@delete')->middleware('isLoggedIn');
-Route::get('add-news', 'NewsController@viewAdd')->middleware('isLoggedIn');
-Route::post('save-news', 'NewsController@save')->middleware('isLoggedIn');
+    // News Management
+    Route::get('all-news', 'NewsController@viewAll');
+    Route::get('edit-news/{id}', 'NewsController@viewEdit');
+    Route::post('update-news/{id}', 'NewsController@update');
+    Route::post('delete-news/{id}', 'NewsController@delete');
+    Route::get('add-news', 'NewsController@viewAdd');
+    Route::post('save-news', 'NewsController@save');
 
-// Comment Management
-Route::get('all-comment', 'CommentController@viewAll')->middleware('isLoggedIn');
-Route::post('delete-comment/{id}', 'CommentController@delete')->middleware('isLoggedIn');
-
-
-
+    // Comment Management
+    Route::get('all-comment', 'CommentController@viewAll');
+    Route::post('delete-comment/{id}', 'CommentController@delete');
+});
 
 
 
