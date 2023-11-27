18d47951e19fea81ba1c38308d05ba9b044558e4
midorimato0082 <86837324+midorimato0082@users.noreply.github.com>
2023-11-27 15:11:24 +0700

    Revert "Merge branch 'main' into develop"
    This reverts commit 1c5177bb0cd49467dd2fee2c405462dfd1c8e85f, reversing
    changes made to 3efdebd92cd1a093540cc57775dbc5da2be8f1c9.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
