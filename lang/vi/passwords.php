ea4efe55016bfd1709abc2ceed69f134fed09845
midorimato0082 <86837324+midorimato0082@users.noreply.github.com>
2023-11-28 17:14:45 +0700

    Update feature Lang

---
+++ lang/vi/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Đặt lại mật khẩu thành công. Đăng nhập nào.',
+    'sent' => 'Vui lòng đến hộp thư email của bạn để đến link đặt lại mật khẩu nhé.',
+    'throttled' => 'Bạn đã gửi yêu cầu quá nhiều lần liên tục. Vui lòng chờ 1 phút trước khi thử lại.',
+    'token' => 'Mã token đặt lại mật khẩu này không tồn tại hoặc mã đã hết hiệu lực.',
+    'user' => "Email này không có trong hệ thống.",
+
+];
